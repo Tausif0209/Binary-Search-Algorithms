a17dc90f120a56ade3101d55686ef421a7291c14
Tausif0209 <tk027592@gmail.com>
2025-05-02 17:24:52 +0530

    GFG: Biotonic Point
    Given an array of integers arr[] that is first strictly increasing and then maybe strictly decreasing, find the bitonic point, that is the maximum element in the array.
    Bitonic Point is a point before which elements are strictly increasing and after which elements are strictly decreasing.
    
    Note: It is guaranteed that the array contains exactly one bitonic point.
    
    Examples:
    
    Input: arr[] = [1, 2, 4, 5, 7, 8, 3]
    Output: 8
    Explanation: Elements before 8 are strictly increasing [1, 2, 4, 5, 7] and elements after 8 are strictly decreasing [3].
    Input: arr[] = [10, 20, 30, 40, 50]
    Output: 50
    Explanation: Elements before 50 are strictly increasing [10, 20, 30 40] and there are no elements after 50.
    Input: arr[] = [120, 100, 80, 20, 0]
    Output: 120
    Explanation: There are no elements before 120 and elements after 120 are strictly decreasing [100, 80, 20, 0].
    Constraints:
    3 ≤ arr.size() ≤ 105
    1 ≤ arr[i] ≤ 106

---
+++ BiotonicPoint.java
@@ -0,0 +1,10 @@
+public class BiotonicPoint {
+  public int findMaximum(int[] arr) {
+    int start=0,end=arr.length-1;
+    while(start<end){
+        int mid=start - (start-end)/2;
+        if(arr[mid]>arr[mid+1]) end=mid;
+        else start=mid+1;
+    } return arr[start];
+ }
+}
